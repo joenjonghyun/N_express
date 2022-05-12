41c69c392b48cd7a07297a1a44ef906d3b5c72fb
joenjonghyun <jjh980814@naver.com>
2022-05-13 01:26:21 +0900

    login update

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import UserModel from './userModel.js'
-import TodoModel from './todoModel.js'
+import UserModel from './User.js'
 mongoose.Promise = global.Promise
 
 const db = {}
 db.mongoose = mongoose
 db.url = dotenv.MONGO_URI
 db.User = new UserModel(mongoose)
-db.Todo = new TodoModel(mongoose)
 
 export default db
